8f63f6cf390f52f83777eac60147910b12af2996
kamangir <arash@kamangir.net>
2025-02-12 22:34:49 -0800

    test++ - kamangir/bolt#746

---
+++ roofai/tests/test_google_maps_geocode.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+
+from blue_options import string
+from blue_objects import objects
+
+from roofai import env
+from roofai.google_maps.geocoding import geocode
+
+
+@pytest.mark.parametrize(
+    ["address"],
+    [["350 W Georgia St, Vancouver, BC V6B 6B1, Canada"]],
+)
+def test_google_maps_geocode(
+    address: str,
+):
+    for object_name in [
+        "",
+        objects.unique_object("test_google_maps_geocode"),
+    ]:
+        success, lat, lon, _ = geocode(
+            address=address,
+            object_name=object_name,
+            verbose=True,
+        )
+        assert success
+        assert isinstance(lat, float)
+        assert isinstance(lon, float)
