7ff983bfd6187146045219ff1d2c3a8cc6de9909
kamangir <arash@kamangir.net>
2025-02-17 22:31:08 -0800

    ++ - kamangir/bolt#746

--- roofai/tests/test_google_maps_predict.py
+++ roofai/tests/test_google_maps_predict.py
@@ -20,7 +20,7 @@ def test_google_maps_predict(
     lat: float,
     lon: float,
 ):
-    prediction_object_name = objects.unique_object(test_google_maps_predict)
+    prediction_object_name = objects.unique_object("test_google_maps_predict")
 
     model_object_name = env.ROOFAI_DEFAULT_GOOGLE_MAPS_MODEL
     assert objects.download(model_object_name)
