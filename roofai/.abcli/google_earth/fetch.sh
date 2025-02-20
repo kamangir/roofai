4bf4d3364e0d928ff3a1115e454eb49bfb2b0ffb
kamangir <arash@kamangir.net>
2025-02-19 21:19:04 -0800

    ++ - kamangir/bolt#746

--- roofai/.abcli/google_earth/fetch.sh
+++ roofai/.abcli/google_earth/fetch.sh
@@ -9,8 +9,7 @@ function roofai_google_earth_fetch() {
 
     local object_name=$(abcli_clarify_object $2 fetch-$lat-$lon-$(abcli_string_timestamp_short))
 
-    local path=$(python3 -m roofai locate)/google_earth/gltf/
-    abcli_eval dryrun=$do_dryrun,path=$path \
+    abcli_eval dryrun=$do_dryrun,path=$ROOFAI_PATH_GLTF \
         node fetch-tiles.js \
         $lat \
         $lon \
--- roofai/.abcli/google_earth/install.sh
+++ roofai/.abcli/google_earth/install.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 
+export ROOFAI_PATH_GLTF=$abcli_path_git/google-earth-as-gltf/simple-node-example/
+
 function abcli_install_roofai_google_earth() {
+    abcli_git_clone git@github.com:kamangir/google-earth-as-gltf.git
 
-    abcli_git_clone https://github.com/OmarShehata/google-earth-as-gltf.git
-
-    local path=$(python3 -m roofai locate)/google_earth/gltf/
-    abcli_eval path=$path \
+    abcli_eval path=$ROOFAI_PATH_GLTF \
         npm install
 }
 
-abcli_install_module roofai_google_earth 1.1.1
+abcli_install_module roofai_google_earth 1.4.1
